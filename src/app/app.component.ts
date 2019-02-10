613eeba156edadb1fb1a5110c343c7236ad0efdb
jirawan charoensuk <jirawan@go.buu.ac.th>
2019-02-10 14:34:34 +0700

    'testbranch'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Card } from './card';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  cards:Card[] = []; // create blank array of Card from card.ts file
+  get sortd():Card[]{
+    console.log('access');
+    return this.cards.sort((a, b)=>b.votes-a.votes);
+
+  }
+
+
+  onclick(firstname,lastname:HTMLInputElement){
+    //alert("Hello Angular");
+    console.log(firstname.value,lastname.value);
+    const card = new Card(firstname.value,lastname.value,0);
+    this.cards.push(card); //insert at last of array
+    console.log(this.cards);
+
+    firstname.value ='';
+    lastname.value = '';
+
+  }
 }
